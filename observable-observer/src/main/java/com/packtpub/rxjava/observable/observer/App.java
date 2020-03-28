7c9f7289f4c73ff44874da102229bd0e99ce3f14
Douglas Dias Batista <douglasdiasbatista@Douglass-MacBook-Pro.local>
2020-03-28 10:44:45 -0300

    Chapter2 Done

--- observable-observer/src/main/java/com/packtpub/rxjava/observable/observer/App.java
+++ observable-observer/src/main/java/com/packtpub/rxjava/observable/observer/App.java
@@ -1,14 +1,16 @@
 package com.packtpub.rxjava.observable.observer;
 
 import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.*;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.observables.ConnectableObservable;
 import lombok.SneakyThrows;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
@@ -118,6 +120,74 @@ public class App {
         source.connect();
     }
 
-    // stop 56 pg
+    @Test
+    public void ch02_15() {
+        Observable.range(1, 3).subscribe(out::println);
+        Observable.range(5, 3).subscribe(out::println);
+    }
 
+    @Test
+    public void ch02_16() {
+        Future<String> v = Executors.newSingleThreadExecutor().submit(() -> "Tears on darkness");
+        Observable.fromFuture(v).subscribe(out::println);
+    }
+
+    // Observable.empty() -> with onCompleted method
+    // Observable.never() -> without onComplete method
+    // Observable.error()
+
+    @Test
+    public void ch02_24() {
+        // static int count = 5;
+        var source1 = Observable.defer(() -> Observable.range(1, 3)); // in a stateful context defer the state
+        // source will be to 5
+        // count = 10
+        // source will changed to 10
+
+    }
+
+    @Test
+    public void ch2_25() {
+        Observable.fromCallable(() -> 1 / 0).subscribe(out::println, out::println); // err.toString
+    }
+
+    @Test
+    public void ch02_26() {
+        Single.just("1").subscribe(out::println); // can be just one parameter
+        Maybe.just(100).subscribe(out::println); // prints 100
+        Completable.fromRunnable(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).subscribe(() -> out.println("Done"));
+    }
+
+    // Disposable to stop Emissions
+
+    @Test
+    public void ch02_36() {
+
+        var source = Observable.create(emitter -> {
+            try {
+
+                for (int i = 0; i < 1000; i++) {
+                    while (!emitter.isDisposed()) {
+                        emitter.onNext(i);
+                    }
+                }
+
+                if (emitter.isDisposed()) {
+                    return;
+                }
+
+                emitter.onComplete();
+
+
+            } catch (Throwable e) {
+                emitter.onError(e);
+            }
+        });
+    }
 }
